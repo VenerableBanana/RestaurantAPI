1bf43e6f904b25e4b37509e65ebd2a755815bfed
Rafał Rywczyński <rr881@wp.pl>
2021-03-11 20:38:28 +0100

    Add project files.

---
+++ RestaurantAPI/RestaurantSeeder.cs
@@ -0,0 +1,95 @@
+﻿using RestaurantAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI
+{
+    public class RestaurantSeeder
+    {
+        private readonly RestaurantDbContext _dbContext;
+        public RestaurantSeeder(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Seed()
+        {
+            if (_dbContext.Database.CanConnect())
+            {
+                if (!_dbContext.Restaurants.Any())
+                {
+                    var restaurants = GetRestaurants();
+                    _dbContext.Restaurants.AddRange(restaurants);
+                    _dbContext.SaveChanges();
+                }
+            }
+        }
+
+        private IEnumerable<Restaurant> GetRestaurants()
+        {
+            var restaurants = new List<Restaurant>()
+            {
+                new Restaurant()
+                {
+                    Name = "KFC",
+                    Category = "Fast Food",
+                    Desctiprion = "American fast food",
+                    ContactEmail = "contact@kfc.com",
+                    HasDelivery = true,
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "Longer",
+                            Price = 10.30M
+                        },
+
+                        new Dish()
+                        {
+                            Name = "Nuggets",
+                            Price = 5.30M
+                        }
+                    },
+                    Address = new Address()
+                    {
+                        City = "Kraków",
+                        Street = "Długa 5",
+                        PostalCode = "30-001"
+                    }
+                },
+                new Restaurant()
+                {
+                    Name = "McDonald",
+                    Category = "Fast Food",
+                    Desctiprion = "American fast food",
+                    ContactEmail = "contact@mcdonald.com",
+                    HasDelivery = true,
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "Hamburger",
+                            Price = 5M
+                        },
+
+                        new Dish()
+                        {
+                            Name = "Nuggets",
+                            Price = 5.30M
+                        }
+                    },
+                    Address = new Address()
+                    {
+                        City = "Warszawa",
+                        Street = "Al.Solidarności 84",
+                        PostalCode = "01-003"
+                    }
+                }
+            };
+
+            return restaurants;
+        }
+
+    }
+}
