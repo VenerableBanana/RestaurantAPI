a865e1fc8a77af8bf238f368a7c61b05a79afb89
Rafał Rywczyński <rr881@wp.pl>
2021-03-12 19:11:30 +0100

    Update implemented

--- RestaurantAPI/Services/RestaurantService.cs
+++ RestaurantAPI/Services/RestaurantService.cs
@@ -16,6 +16,7 @@ namespace RestaurantAPI.Services
         IEnumerable<RestaurantDto> GetAll();
         int Create(CreateRestaurantDto dto);
         bool Delete(int id);
+        bool Update(int id, UpdateRestaurantDto dto);
     }
 
     public class RestaurantService : IRestaurantService
@@ -27,6 +28,23 @@ namespace RestaurantAPI.Services
         {
             _dbContext = dbContext;
             _mapper = mapper;
+        }
+
+        public bool Update(int id, UpdateRestaurantDto dto)
+        {
+            var restaurant = _dbContext
+                .Restaurants
+                .FirstOrDefault(r => r.Id == id);
+
+            if (restaurant is null) return false;
+
+            restaurant.Name = dto.Name;
+            restaurant.Desctiprion = dto.Description;
+            restaurant.HasDelivery = dto.HasDelivery;
+
+            _dbContext.SaveChanges();
+
+            return true;
         }
 
         public bool Delete(int id)
