4020f7067f79545e951f79d3b1bff322c28b3cb5
Rafał Rywczyński <rr881@wp.pl>
2021-03-15 18:46:02 +0100

    Users, Roles and adding users

---
+++ RestaurantAPI/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Models;
+using RestaurantAPI.Services;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accoutService;
+
+        public AccountController(IAccountService accoutService)
+        {
+            _accoutService = accoutService;
+        }
+
+        [HttpPost("register")]
+        public ActionResult RegisterUser([FromBody]RegisterUserDto dto)
+        {
+            _accoutService.RegisterUser(dto);
+            return Ok();
+        }
+    }
+}
