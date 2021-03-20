05333c4a3901abed445b6a3e15cbdd14fe1964df
Rafał Rywczyński <rr881@wp.pl>
2021-03-20 16:49:54 +0100

    Uploading and downloading files

---
+++ RestaurantAPI/Controllers/FileController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("file")]
+    [Authorize]
+    public class FileController : ControllerBase
+    {
+        public ActionResult GetFile([FromQuery] string fileName)
+        {
+            var rootPath = Directory.GetCurrentDirectory();
+
+            var filePath = $"{rootPath}/PrivateFiles/{fileName}";
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound();
+            }
+
+            var contentProvider = new FileExtensionContentTypeProvider();
+            contentProvider.TryGetContentType(fileName, out string contentType);
+
+            var fileContents = System.IO.File.ReadAllBytes(filePath);
+
+            return File(fileContents, contentType, fileName);
+        }
+
+        [HttpPost]
+        public ActionResult Upload([FromForm]IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                var rootPath = Directory.GetCurrentDirectory();
+                var fileName = file.FileName;
+                var fullPath = $"{rootPath}/PrivateFiles/{fileName}";
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+    }
+}
